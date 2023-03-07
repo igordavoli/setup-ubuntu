d555d3befb1c012604b58e27b21360d1dde42687
igordavoli <igordavoli@gmail.com>
2023-03-07 00:22:06 -0300

    add download and set wallpaper

--- configure.sh
+++ configure.sh
@@ -8,62 +8,67 @@ SSH_DIR=$HOME/.ssh
 USERNAME=igordavoli
 
 SSH_CONFIG_CONTENT=\
-"Host github.com
-    HostName github.com
-    User git
-    IdentityFile ~/.ssh/github
-"
+	"Host github.com
+	    HostName github.com
+	    User git
+	    IdentityFile ~/.ssh/github
+	"
 PACKEGES=(
-"build-essential"
-"curl"
-"fonts-firacode"
-"git"
-"gnome-tweaks"
-"nodejs"
-"npm"
-"vlc"
+	"build-essential"
+	"curl"
+	"fonts-firacode"
+	"git"
+	"gnome-tweaks"
+	"nodejs"
+	"npm"
+	"vlc"
 # "corectrl"
 )
 
 SNAPS=(
-"code --classic" 
-"dbeaver-ce" 
-"discord" 
-"insomnia" 
-"spotify"
+	"code --classic" 
+	"dbeaver-ce" 
+	"discord" 
+	"insomnia" 
+	"spotify"
 )
 
 VSCODE_EXTENSIONS=(
-"cschlosser.doxdocgen"
-"dbaeumer.vscode-eslint"
-"dracula-theme.theme-dracula"
-"eamodio.gitlens"
-"esbenp.prettier-vscode"
-"foxundermoon.shell-format"
-"MS-vsliveshare.vsliveshare"
-"PKief.material-icon-theme"
-"redhat.vscode-yaml"
-"streetsidesoftware.code-spell-checker"
-"streetsidesoftware.code-spell-checker-portuguese-brazilian"
-"styled-components.vscode-styled-components"
-"TabNine.tabnine-vscode"
-"ue.alphabetical-sorter"
+	"cschlosser.doxdocgen"
+	"dbaeumer.vscode-eslint"
+	"dracula-theme.theme-dracula"
+	"eamodio.gitlens"
+	"esbenp.prettier-vscode"
+	"foxundermoon.shell-format"
+	"MS-vsliveshare.vsliveshare"
+	"PKief.material-icon-theme"
+	"redhat.vscode-yaml"
+	"streetsidesoftware.code-spell-checker"
+	"streetsidesoftware.code-spell-checker-portuguese-brazilian"
+	"styled-components.vscode-styled-components"
+	"TabNine.tabnine-vscode"
+	"ue.alphabetical-sorter"
 )
 
 GNOME_CONFIGS=(
-"desktop.interface gtk-theme 'Nordic'"
-"desktop.interface icon-theme 'Papirus'"
-"desktop.wm.preferences button-layout 'close,minimize,maximize:'"
-"shell.extensions.dash-to-dock dash-max-icon-size 42"
-"shell.extensions.dash-to-dock dock-position 'BOTTOM'"
-"shell.extensions.dash-to-dock extend-height false"
-"shell.extensions.dash-to-dock isolate-monitors true"
-"shell.extensions.dash-to-dock isolate-workspaces true"
-"shell favorite-apps ['snap-store_ubuntu-software.desktop', 'org.gnome.Nautilus.desktop', 'code_code.desktop', 'google-chrome.desktop', 'discord_discord.desktop', 'dbeaver-ce_dbeaver-ce.desktop', 'insomnia_insomnia.desktop', 'vlc.desktop', 'spotify_spotify.desktop']"
+	"desktop.interface gtk-theme 'Nordic'"
+	"desktop.interface icon-theme 'Papirus'"
+	"desktop.wm.preferences button-layout 'close,minimize,maximize:'"
+	"desktop.background picture-uri "/home/igordavoli/Pictures/Wallpapers/montains.jpeg"" 
+	"shell.extensions.dash-to-dock dash-max-icon-size 36"
+	"shell.extensions.dash-to-dock dock-position 'BOTTOM'"
+	"shell.extensions.dash-to-dock extend-height false"
+	"shell.extensions.dash-to-dock dock-fixed false" 
+	"shell.extensions.dash-to-dock isolate-monitors true"
+	"shell.extensions.dash-to-dock isolate-workspaces true"
+	"shell.extensions.dash-to-dock show-mounts false"
+	"shell.extensions.dash-to-dock show-mounts-network false"
+	"shell.extensions.dash-to-dock show-mounts-only-mounted true"
+	"shell favorite-apps ['snap-store_ubuntu-software.desktop', 'org.gnome.Nautilus.desktop', 'code_code.desktop', 'google-chrome.desktop', 'discord_discord.desktop', 'dbeaver-ce_dbeaver-ce.desktop', 'insomnia_insomnia.desktop', 'vlc.desktop', 'spotify_spotify.desktop']"
 )
 
 # update and upgrade packages
-sudo add-apt-repository ppa:ernstp/mesarc -y
+# sudo add-apt-repository ppa:ernstp/mesarc -y
 sudo apt update -y
 sudo apt upgrade -y
 
@@ -110,13 +115,15 @@ mv Nordic/ /usr/share/themes/
 add-apt-repository ppa:papirus/papirus
 apt install papirus-icon-theme -y
 
-# # set theme configs
+# set theme configs
+wget -c https://i.imgur.com/SBKjnxm.jpeg -O /home/igordavoli/Pictures/Wallpapers/montains.jpeg
+
 for ((i = 0; i < ${#GNOME_CONFIGS[@]}; i++))
 do
 	gsettings set org.gnome.${GNOME_CONFIGS[$i]}
 done
 
-# fix cedilla isue in english intl keyboards
+# fix cedilla issue in english intl keyboards
 echo GTK_IM_MODULE=cedilla | sudo tee -a  /etc/environment > /dev/null
 
 # set corectrl to initialize with startup and don't request root permissions
