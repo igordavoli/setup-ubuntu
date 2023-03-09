68667494e882bbef7a57c5fd2c5ddd6887293ff0
Igor Davoli <igordavoli@gmail.com>
2023-03-09 19:54:05 -0300

    add set vscode configs and missing sudos

--- configure.sh
+++ configure.sh
@@ -26,10 +26,10 @@ PACKEGES=(
 )
 
 SNAPS=(
-	"code --classic" 
-	"dbeaver-ce" 
-	"discord" 
-	"insomnia" 
+	"code --classic"
+	"dbeaver-ce"
+	"discord"
+	"insomnia"
 	"spotify"
 )
 
@@ -48,24 +48,36 @@ VSCODE_EXTENSIONS=(
 	"styled-components.vscode-styled-components"
 	"TabNine.tabnine-vscode"
 	"ue.alphabetical-sorter"
+	"sohamkamani.code-eol"
 )
+
+VSCODE_OPTIONS="{
+    \"tabnine.experimentalAutoImports\": true,
+	\"workbench.colorTheme\": \"Dracula\",
+    \"editor.fontFamily\": \"Fira Code\",
+    \"files.insertFinalNewline\":true,
+    \"editor.renderWhitespace\": \"all\",
+    \"files.trimTrailingWhitespace\": true,
+    \"files.trimFinalNewlines\": true,
+	\"material-icon-theme.activeIconPack\": \"nest\",
+    \"files.eol\": \"\n\"
+}"
 
 GNOME_CONFIGS=(
 	"desktop.interface gtk-theme 'Nordic'"
 	"desktop.interface icon-theme 'Papirus'"
 	"desktop.wm.preferences button-layout 'close,minimize,maximize:'"
-	"desktop.background picture-uri "/home/igordavoli/Pictures/Wallpapers/montains.jpeg"" 
+	"desktop.background picture-uri '/home/igordavoli/Pictures/Wallpapers/montains.jpeg'"
 	"shell.extensions.dash-to-dock dash-max-icon-size 36"
 	"shell.extensions.dash-to-dock dock-position 'BOTTOM'"
 	"shell.extensions.dash-to-dock extend-height false"
-	"shell.extensions.dash-to-dock dock-fixed false" 
+	"shell.extensions.dash-to-dock dock-fixed false"
 	"shell.extensions.dash-to-dock isolate-monitors true"
 	"shell.extensions.dash-to-dock isolate-workspaces true"
 	"shell.extensions.dash-to-dock show-mounts false"
 	"shell.extensions.dash-to-dock show-mounts-network false"
 	"shell.extensions.dash-to-dock show-mounts-only-mounted true"
 	"shell.extensions.ding show-home false"
-	"shell favorite-apps ['snap-store_ubuntu-software.desktop', 'org.gnome.Nautilus.desktop', 'code_code.desktop', 'google-chrome.desktop', 'discord_discord.desktop', 'dbeaver-ce_dbeaver-ce.desktop', 'insomnia_insomnia.desktop', 'vlc.desktop', 'spotify_spotify.desktop']"
 )
 
 # update and upgrade packages
@@ -76,7 +88,7 @@ sudo apt upgrade -y
 # install packages
 for ((i = 0; i < ${#PACKEGES[@]}; i++))
 do
-	sudo snap install ${PACKEGES[$i]}
+	sudo apt install ${PACKEGES[$i]} -y
 done
 
 # install snaps
@@ -91,8 +103,11 @@ do
 	code --install-extension ${VSCODE_EXTENSIONS[$i]}
 done
 
-# install n 
-sudo npm i n
+# set vscode configs
+echo -e $VS_CODE_VSCODE_OPTIONS | tee -a $HOME/.config/Code/User/settings.json > /dev/null
+
+# install n
+sudo npm i -g n
 
 # install node lts
 sudo n lts
@@ -105,18 +120,19 @@ npm i yarn
 
 # install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb -y
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm ./google-chrome-stable_current_amd64.deb
 
 # install nordic theme
 git clone https://github.com/EliverLara/Nordic.git
-mv Nordic/ /usr/share/themes/
+sudo mv Nordic/ /usr/share/themes/
 
 # install papurus icons
-add-apt-repository ppa:papirus/papirus
-apt install papirus-icon-theme -y
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt install papirus-icon-theme -y
 
 # set theme configs
+mkdir -p Pictures/Wallpapers
 wget -c https://i.imgur.com/SBKjnxm.jpeg -O /home/igordavoli/Pictures/Wallpapers/montains.jpeg
 
 for ((i = 0; i < ${#GNOME_CONFIGS[@]}; i++))
@@ -124,22 +140,23 @@ do
 	gsettings set org.gnome.${GNOME_CONFIGS[$i]}
 done
 
+gsettings set org.gnome.shell favorite-apps "['snap-store_ubuntu-software.desktop', 'org.gnome.Nautilus.desktop', 'code_code.desktop', 'google-chrome.desktop', 'discord_discord.desktop', 'dbeaver-ce_dbeaver-ce.desktop', 'insomnia_insomnia.desktop', 'vlc.desktop', 'spotify_spotify.desktop']"
 # fix cedilla issue in english intl keyboards
 echo GTK_IM_MODULE=cedilla | sudo tee -a  /etc/environment > /dev/null
 
 # set corectrl to initialize with startup and don't request root permissions
 # cp /usr/share/applications/org.corectrl.corectrl.desktop ~/.config/autostart/org.corectrl.corectrl.desktop
-# echo \ 
+# echo \
 # "[User permissions]
 # Identity=unix-group:$USERNAME
 # Action=org.corectrl.*
 # ResultActive=yes" \
-#  | sudo tee -a  /etc/polkit-1/localauthority/50-local.d/90-corectrl.pkla > /dev/null
+# | sudo tee -a  /etc/polkit-1/localauthority/50-local.d/90-corectrl.pkla > /dev/null
 
 # GRUB_CMDLINE_LINUX_DEFAULT="<other_params>... amdgpu.ppfeaturemask=0xffffffff"
 # grub-mkconfig -o /boot/grub/grub.cfg
 
 # generate github ssh key
 ssh-keygen -t rsa -b 4096 -C $GIT_HUB_EMAIL -f $SSH_DIR/github
-touch $SSH_DIR/config && echo -e $SSH_CONFIG_CONTENT> SSH_DIR/config 
+touch $SSH_DIR/config && echo -e $SSH_CONFIG_CONTENT> SSH_DIR/config
 cat SSH_DIR/github.pub
